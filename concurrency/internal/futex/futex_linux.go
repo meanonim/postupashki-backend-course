d933dbce052048e7de76899b4f899295e5408e30
Tim <aisles.velds.02@icloud.com>
2026-09-21 00:17:05 +0300

    fix futex wait

--- concurrency/internal/futex/futex_linux.go
+++ concurrency/internal/futex/futex_linux.go
@@ -25,5 +25,5 @@ func Wake(addr *uint32) {
 
 func WakeAll(addr *uint32) {
 	syscall.Syscall6(syscall.SYS_FUTEX, uintptr(unsafe.Pointer(addr)),
-		opWake|private, ^uintptr(0)>>1, 0, 0, 0)
+		opWake|private, 1<<31-1, 0, 0, 0)
 }
